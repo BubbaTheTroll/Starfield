4f52fceb61b6980424d2f6ce466af0175930cd51
alcai3 <alcai3@697-334-Forlond.sf.k12.internal>
2014-10-21 10:57:06 -0700

    Added first name to index.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] Trippy;
 
 
 //OddballParticle[] Cai;//your code here
-void setup()
+public void setup()
 {
 	size(500,500);
 	Trippy = new Particle [1000];
@@ -13,7 +29,7 @@ void setup()
 	Trippy[1] = new OddballParticle();
 	
 }
-void draw()
+public void draw()
 {
 		Trippy[1].move();
 		Trippy[1].show();
@@ -97,3 +113,12 @@ interface Particle
 	public void move();//your code here
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
